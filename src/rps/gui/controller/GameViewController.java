ff352605a9d10559c590bf41847e9fd5ac99fbb5
Mareike0308 <mare1702@easv365.dk>
2022-03-02 12:29:13 +0100

    I hate the toilet paper guy

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -2,6 +2,9 @@ package rps.gui.controller;
 
 // Java imports
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,11 +14,21 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    public ImageView misterCrabsimg;
+    public ImageView theRockimg;
+    public ImageView thisGuyimg;
+    public ImageView bananaBackground1;
+    public ImageView bananaBackgroud2;
+
+
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
+
+
     }
 }
